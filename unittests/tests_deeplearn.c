e4c13c22a1421be95ec89a191fd5d8a2d3d7904c
Bob Mottram <bob@robotics.uk.to>
2015-03-15 23:03:25 +0000

    Date range

--- unittests/tests_deeplearn.c
+++ unittests/tests_deeplearn.c
@@ -1,5 +1,5 @@
 /*
- Copyright (C) 2013  Bob Mottram <bob@robotics.uk.to>
+ Copyright (C) 2013-2015  Bob Mottram <bob@robotics.uk.to>
 
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions
