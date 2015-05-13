d7a6606a656908c58746ab0dcd398dde93cd534e
Bob Mottram <bob@robotics.uk.to>
2015-05-13 22:14:57 +0100

    Test for learning features from floats

--- unittests/tests_features.c
+++ unittests/tests_features.c
@@ -66,12 +66,12 @@ static void test_learn_from_image()
     int no_of_inputs = samples_across*samples_down*no_of_features;
 
     /* create a network */
-	feature_autocoder = (bp*)malloc(sizeof(bp));
+    feature_autocoder = (bp*)malloc(sizeof(bp));
     assert(bp_init(feature_autocoder,
-				   patch_radius*patch_radius*4*image_depth,
-				   no_of_features,1,
-				   patch_radius*patch_radius*4*image_depth,
-				   &random_seed) == 0);
+                   patch_radius*patch_radius*4*image_depth,
+                   no_of_features,1,
+                   patch_radius*patch_radius*4*image_depth,
+                   &random_seed) == 0);
     assert(feature_autocoder->inputs!=0);
     assert(feature_autocoder->hiddens!=0);
     assert(feature_autocoder->outputs!=0);
@@ -106,8 +106,98 @@ static void test_learn_from_image()
 
     /* free the memory */
     bp_free(feature_autocoder);
-	free(feature_autocoder);
+    free(feature_autocoder);
     free(img);
+
+    printf("Ok\n");
+}
+
+static void test_learn_from_flt()
+{
+    printf("test_learn_from_flt...");
+
+    bp * feature_autocoder;
+    unsigned int image_width = 10;
+    unsigned int image_height = 10;
+    int image_depth = 24/8;
+    int result,i = 0;
+    unsigned int random_seed = 123;
+    float error_value[10];
+    unsigned int bitsperpixel = 0;
+    unsigned char * img, * img2;
+    float flt[128*128*3];
+    float BPerror=0;
+
+    /* load image from file */
+    assert(deeplearn_read_png_file((char*)"Lenna.png", &image_width, &image_height, &bitsperpixel, &img)==0);
+
+    assert(image_width == 512);
+    assert(image_height == 512);
+    assert(bitsperpixel == 24);
+
+    img2 = (unsigned char*)malloc(128*128*3*sizeof(unsigned char));
+    assert(img2);
+    deeplearn_downsample(img, image_width, image_height,
+                         img2, 128, 128);
+    free(img);
+    img = img2;
+    image_width = 128;
+    image_height = 128;
+
+    /* convert to an array of floats */
+    for (int i = 0; i < 128*128*3; i++) {
+        flt[i] = 0.25f + ((float)img[i]/(255.0*2));
+    }
+    free(img);
+
+    int patch_radius = 4;
+    int samples_across = image_width/patch_radius;
+    int samples_down = image_height/patch_radius;
+    int no_of_features = 6*6;
+    int no_of_inputs = samples_across*samples_down*no_of_features;
+
+    /* create a network */
+    feature_autocoder = (bp*)malloc(sizeof(bp));
+    assert(bp_init(feature_autocoder,
+                   patch_radius*patch_radius*4*image_depth,
+                   no_of_features,1,
+                   patch_radius*patch_radius*4*image_depth,
+                   &random_seed) == 0);
+    assert(feature_autocoder->inputs!=0);
+    assert(feature_autocoder->hiddens!=0);
+    assert(feature_autocoder->outputs!=0);
+    assert(feature_autocoder->BPerror == DEEPLEARN_UNKNOWN_ERROR);
+    assert(feature_autocoder->BPerrorAverage == DEEPLEARN_UNKNOWN_ERROR);
+    assert(feature_autocoder->BPerrorTotal == DEEPLEARN_UNKNOWN_ERROR);
+
+    for (i = 0; i < 8; i++) {
+        result =
+            features_learn_from_flt(samples_across,
+                                    samples_down,
+                                    patch_radius,
+                                    image_width,
+                                    image_height,
+                                    image_depth,
+                                    &flt[0],
+                                    no_of_inputs,
+                                    feature_autocoder,
+                                    &BPerror);
+        if (result != 0) {
+            printf("\nresult = %d\n",result);
+        }
+        assert(result==0);
+        error_value[i] = BPerror;
+    }
+
+    /* check that the training error reduced */
+    assert(error_value[6] + error_value[7] < error_value[0] + error_value[1]);
+    bp_plot_weights(feature_autocoder,
+                    "/tmp/test_features_learn_from_flt.png",
+                    480,800,8);
+
+    /* free the memory */
+    bp_free(feature_autocoder);
+    free(feature_autocoder);
 
     printf("Ok\n");
 }
@@ -117,6 +207,7 @@ int run_tests_features()
     printf("\nRunning feature learning tests\n");
 
     test_learn_from_image();
+    test_learn_from_flt();
 
     printf("All feature learning tests completed\n");
     return 1;
