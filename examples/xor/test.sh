5aaa3a3a41ed45156331594e433fd6f3312c9d3f
Bob Mottram <bob@robotics.uk.to>
2015-06-09 20:37:20 +0100

    Test test inputs for xor example

--- examples/xor/test.sh
+++ examples/xor/test.sh
@@ -2,13 +2,13 @@
 make
 ./xor
 gcc -o export_xor_c export_xor.c -lm
-./export_xor_c 0 0
-./export_xor_c 1 0
-./export_xor_c 0 1
-./export_xor_c 1 1
+./export_xor_c zero zero
+./export_xor_c one zero
+./export_xor_c zero one
+./export_xor_c one one
 echo ""
-./export_xor.py 0 0
-./export_xor.py 1 0
-./export_xor.py 0 1
-./export_xor.py 1 1
+./export_xor.py zero zero
+./export_xor.py one zero
+./export_xor.py zero one
+./export_xor.py one one
 exit
