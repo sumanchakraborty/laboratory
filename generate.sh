642b9ba88cf570a5ae13217e8287253811a08c31
Bob Mottram <bob@robotics.uk.to>
2015-10-28 14:19:38 +0000

    Remove packaging generator

--- generate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Generates packaging for packagemonkey
-
-rm -f Makefile rpmpackage/libdeep.spec
-
-packagemonkey -n "libdeep" --version "1.00" --cmd --dir "." -l "bsd" -e "Bob Mottram (4096 bits) <bob@robotics.uk.to>" --brief "C Library for deep learning" --desc "Makes using deep learning easy to include within any C/C++ application." --homepage "https://github.com/bashrc/libdeep" --repository "https://github.com/bashrc/libdeep.git" --section "libs" --categories "Development/ArtificialIntelligence" --cstandard "c99" --compile "-lm -fopenmp" --dependsdeb "gnuplot, doxygen" --dependsarch "gnuplot, doxygen" --dependsrpm "gnuplot, doxygen"
