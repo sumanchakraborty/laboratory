37f5acbcd12b3b9e866df821b421e108ad54233d
Suman Chakraborty <suman.chakraborty@gmail.com>
2016-09-22 11:43:28 +0000

    matrix_layer_rotation.c

--- c/matrix_layer_rotation.c
+++ c/matrix_layer_rotation.c
@@ -8,6 +8,20 @@
     printf(__VA_ARGS__);              \
 }
 
+/*
+INPUT_FORMAT
+------------
+ row 4, col 4, step 2
+
+ 4 4 2
+ 1 2 3 4
+ 5 6 7 8
+ 9 10 11 12
+ 13 14 15 16
+
+ https://www.hackerrank.com/challenges/matrix-rotation-algo
+*/
+
 struct layer {
     int r_count; // row
     int c_count; // column
