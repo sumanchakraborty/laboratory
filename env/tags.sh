8d1224fe8d169f4f3ef33d2a7bcab1d5204058c3
Suman Chakraborty <sumanchakraborty@users.noreply.github.com>
2017-11-14 16:11:17 +0530

    Update tags.sh

--- env/tags.sh
+++ env/tags.sh
@@ -1,6 +1,19 @@
-touch tags.lst
-find | grep "\.hpp$" >> tags.lst
-find | grep "\.hxx$" >> tags.lst
-find | grep "\.cpp$" >> tags.lst
-ctags -R --exclude=.git .
+echo "removing old stuff ..."
+rm -rf cscope.out tags tags.lst
+
+echo "creating new stuff ..."
+find | egrep -i "\.(h|hpp)$" >> tags.lst
+find | egrep -i "\.(c|cpp)$" >> tags.lst
+find | egrep -i "\.(hxx)$" >> tags.lst
+sort tags.lst > tags.lst.sorted
+mv tags.lst.sorted tags.lst
+
+echo "executing ctags ..."
+ctags -L tags.lst
+# ctags -R --exclude=.git .
+
+echo "executing cscope ..."
 cscope -i tags.lst
+
+# run this script with following
+# dos2unix tags.sh; chmod +x tags.sh; ./tags.sh
